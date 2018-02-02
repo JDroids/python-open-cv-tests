46e9c60a8c2a0fd78005263a2e87f1592f05bfe7
dansman805 <dansman805@gmail.com>
2018-02-02 15:58:28 -0500

    Worked on stuff (very descriptive, I know)

--- getHistogramFromFile.py
+++ getHistogramFromFile.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
  
-img = cv2.imread('Blue.jpg')
+img = cv2.imread('BlueCropped.jpg')
 hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 b, g, r = cv2.split(img)
@@ -13,5 +13,15 @@ b, g, r = cv2.split(img)
 
 hist = cv2.calcHist(img, [0, 1], None, [180, 256], [0, 180, 0, 256])
 
+histFile = open('RedCroppedHistogram.txt', 'w')
+
+for item in hist:
+    histFile.write(str(item)+'\n')
+
+
+histFile.close()
+
+
 plt.plot(hist)
+
 plt.show()
