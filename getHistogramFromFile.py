5ac889505625c36e7349d8b12ceb444902ed86db
dansman805 <dansman805@gmail.com>
2018-02-03 09:12:19 -0500

    Got automated cropping to work, got it to detect edges

--- getHistogramFromFile.py
+++ getHistogramFromFile.py
@@ -2,16 +2,18 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
  
-img = cv2.imread('BlueCropped.jpg')
+img = cv2.imread('Blue.jpg')
 hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 b, g, r = cv2.split(img)
 
 #TODO: Crop Image
 
+croppedImage = img[1462:1914, 336:1020]
+
 #Maybe use color specific hist
 
-hist = cv2.calcHist(img, [0, 1], None, [180, 256], [0, 180, 0, 256])
+hist = cv2.calcHist(croppedImage, [0, 1], None, [180, 256], [0, 180, 0, 256])
 
 histFile = open('RedCroppedHistogram.txt', 'w')
 
@@ -20,6 +22,15 @@ for item in hist:
 
 
 histFile.close()
+
+
+gray = cv2.cvtColor(croppedImage, cv2.COLOR_BGR2GRAY)
+grayFiltered = cv2.bilateralFilter(gray, 11, 17, 17)
+edged = cv2.Canny(grayFiltered, 30, 200)
+
+cv2.imshow("Cropped Image", croppedImage)
+cv2.imshow("Edged Image", edged)
+
 
 
 plt.plot(hist)
